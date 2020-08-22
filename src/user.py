c32bf1cf1bdd0b1fd0fcabbca8619d9ebd7b432a
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-21 22:09:14 -0700

    update.

--- src/user.py
+++ src/user.py
@@ -8,3 +8,16 @@ class User:
     def add_to_car(self, product):
         self.cart.append(product)
         print(f"added {product.name}")
+
+# dict = {0: 1, 1: 1,2: 2}
+# def fact(n):
+#     index = 2
+#     # if n in dict:
+#     #     return n
+#     for num in range(index, n + 1):
+#         dict[num] = dict[index - 1] * dict[index - 2]
+#         index += 1
+#     return dict[n]
+# print(fact(5))
+
+print(3**(-0))
