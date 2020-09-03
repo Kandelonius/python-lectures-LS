47280f80fe2787e2e423a2542fa7dafdca169b4c
Shane Kaestner <shane.kaestner@yahoo.com>
2020-09-02 22:09:24 -0700

    updated previous lectures.

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -1,11 +1,24 @@
+# say we want to store all of the even numbers in the range
+# 0-100 in a list
+# what are some ways we can do this?
 evens = []
 
+# # loop through the range
 for i in range(101):
     if i % 2 == 0:
         evens.append(i)
 
 print(evens)
 
+# # comprehensions allow us to write the above logic in a much
+# # more terse fashion
+# evens_2 = [i for i in range(101) if i % 2 == 0]
+# # print(evens)
+# print(evens_1 == evens_2)
+
+# create a list of the square values of numbers in the range 1-10
+
+# squares_1 = []
 odds = [n for n in range(101) if n % 2 == 1]
 
 print(f"odds: {odds}")
@@ -16,13 +29,16 @@ print(f"squares: {squares}")
 
 names = ["Sarah", "jorge", "sam", "Jake", "jeremy", "sandy", "shawn"]
 
+# create a new list containing only the names that start with `s`
+# and also make sure all of the names are properly capitalized
 s_names = []
 j_names = []
 
 for name in names:
     if name[0].lower() == 's':
         s_names.append(name.capitalize())
-
+# if it does, add it to `s_names`, making sure the
+    # name is properly capitalized 
 print(s_names)
 # print(names)
 
